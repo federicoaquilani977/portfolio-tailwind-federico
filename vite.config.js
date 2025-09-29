a39bf610a5e4c8e822f0ece0fa35e21818251020
federicoaquilani977 <aquilanifederico@live.it>
2025-09-29 04:10:43 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: '/portfolio-tailwind-federico-main/',
+  base: '/portfolio-tailwind-federico/',
 });
